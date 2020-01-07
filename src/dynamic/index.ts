1f04d847024e5beca8045c8b276cb75affbedbca
pengchaoqun <pengchaoqun@fzxs.com.cn>
2020-01-07 19:36:31 +0800

    add utility

--- src/dynamic/index.ts
+++ src/dynamic/index.ts
@@ -1,35 +1,50 @@
 
-import { chain, Rule, apply, url, template, branchAndMerge, mergeWith } from '@angular-devkit/schematics';
+import { chain, Rule, apply, url, mergeWith, move, applyTemplates, Tree } from '@angular-devkit/schematics';
 import { classify, dasherize, camelize, underscore } from '@angular-devkit/core/src/utils/strings';
 import { SchemaOptions } from './schema';
-import * as path from "path";
+import { createDefaultPath } from '../utility/workspace';
+import { parseName } from '../utility/parse-name';
+// import * as path from "path";
 
 const stringUtils = { classify, dasherize, camelize, underscore };
 
-export function main(options: SchemaOptions): Rule {
+export function main(options: SchemaOptions | any): Rule {
+  return async (host: Tree) => {
+    if (options.path === undefined) {
+      options.path = await createDefaultPath(host, options.project as string);
+    }
 
-  const templateOptions: SchemaOptions = {
-    path: path.resolve('./'),
-    name: options.name,
-    module: options.module,
-    tabTitle: options.tabTitle,
-    searchFormName: options.searchFormName,
-    tableName: options.tableName,
-    newFormName: options.newFormName,
-    editFormName: options.editFormName
-  }
-  console.log(templateOptions)
+    const parsedPath = parseName(options.path, options.name);
+    options.name = parsedPath.name;
+    options.path = parsedPath.path;
+    
+    console.log(`parsedPath`)
+    console.log(parsedPath)
 
-  const templateSource = apply(url('./files'), [
-    template({
-      ...stringUtils,
-      ...templateOptions
-    })
-  ]);
+    const templateOptions: SchemaOptions = {
+      path: process.cwd(),
+      name: options.name,
+      module: options.module,
+      tabTitle: options.tabTitle,
+      searchFormName: options.searchFormName,
+      tableName: options.tableName,
+      newFormName: options.newFormName,
+      editFormName: options.editFormName
+    }
+    console.log(templateOptions)
+    console.log(templateOptions)
 
-  return chain([
-    branchAndMerge(chain([
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        ...stringUtils,
+        ...templateOptions
+      }),
+      move(parsedPath.path)
+    ]);
+
+    // return noop();
+    return chain([
       mergeWith(templateSource)
-    ]))
-  ]);
+    ]);
+  }
 }
